0f6d647853f9d7e8a857ee526759ec3a41e05f77
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2024-10-31 09:16:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// super podstawowy program, na razie nie ma zapisu ani odzczytu
+Console.WriteLine("Licznik Dram. Liczy dramy");
+Console.WriteLine("Czy Drama dziś była? Y - Tak, N - Nie");
+
+
+switch (Console.ReadLine()) {
+
+        case "Y":
+        Console.WriteLine("O! Drama!");
+        break;
+
+        case "N":
+        Console.WriteLine("Nie Bylo Dramy");
+        break;
+ }
+
+Console.WriteLine("Kliknij cos aby wylaczyc");
+Console.ReadKey();
